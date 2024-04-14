3e8aee9848f6781929cb2b50e8afcdc01ba80fca
wackerdam <149610741+wackerdam@users.noreply.github.com>
2024-04-14 15:19:12 +0100

    Update api_image_request.py

--- Basic-API/api_image_request.py
+++ Basic-API/api_image_request.py
@@ -2,14 +2,21 @@ import requests
 from PIL import Image
 from io import BytesIO
 
-def show_image_from_url(url):
-    response = requests.get(url)
-    
-    if response.status_code == 200:
+def display_image_from_url(image_url):
+    """Fetches an image from a URL and displays it.
+
+    Args:
+        image_url (str): The URL of the image to download.
+    """
+
+    response = requests.get(image_url)
+
+    if response.status_code == 200:  # Check for successful retrieval
         image = Image.open(BytesIO(response.content))
         image.show()
     else:
-        print(f"Failed to fetch image from {url}. Status code: {response.status_code}")
+        print(f"Failed to fetch image from {image_url}. Status code: {response.status_code}")
 
-imageurl = input("Enter image url: ")
-show_image_from_url(imageurl)
+if __name__ == "__main__":  # Good practice for scripts
+    image_url = input("Enter image URL: ")
+    display_image_from_url(image_url)
